d3d9a7cb4853fe0eaa6c8673f84eb4d65fe8725a
José Manuel Barja Vázquez <josemanuelbarja@MacBook-Pro-de-Jose-5.local>
2018-10-24 13:50:29 +0200

    Implementado pasar por parametro los datos en client.py

--- client.py
+++ client.py
@@ -5,17 +5,19 @@ Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+server = sys.argv[1]
+port = bytes(sys.argv[2],'utf-8')
+line = sys.argv[3:]
+words = " ".join(line)
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    my_socket.connect((server, int(port)))
+    print("Enviando:", words)
+    my_socket.send(bytes(words, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
